566dad39db545633c11e862b38cd57c1753ee2ba
Harlan Haller <harlanhaller06@gmail.com>
2022-11-12 10:34:18 -0500

    intial commit

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import frc.robot.commands.*;
+
+/** Add your docs here. */
+public class OI {
+    private static final XboxController driverController = new XboxController(0);
+    private static final XboxController manipulatorController = new XboxController(1);
+
+    public OI() {
+        // intake = new Intake();
+    }
+
+    public static DoubleSupplier[] getDriveSuppliers() {
+       return new DoubleSupplier[] {() -> driverController.getLeftY(), () -> -driverController.getRightX()};
+    }
+
+    public void bindButtons() {
+    }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.commands.DriveWithJoystick;
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  public static final Drivetrain drivetrain = new Drivetrain();
+
+  private final OI oi = new OI();
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    initDefaultCommands();
+    oi.bindButtons();
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return new InstantCommand();
+  }
+
+  private void initDefaultCommands() {
+    drivetrain.setDefaultCommand(new DriveWithJoystick());
+  }
+}
